e162e28a1033477eebbf15b3414f558486894804
chenwy <chenwy323@163.com>
2017-04-18 18:46:10 +0800

    implement sign in by username or email

---
+++ db/migrate/20170418104346_change_users_index_email_to_unique.rb
@@ -0,0 +1,6 @@
+class ChangeUsersIndexEmailToUnique < ActiveRecord::Migration[5.0]
+  def change
+    remove_index :users, :email
+    add_index :users, :email, unique: true, name: "index_users_on_email"
+  end
+end
