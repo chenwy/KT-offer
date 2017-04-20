7d68cc6319b9fcc0bcdba4ba123ad0c52ad1df5e
chenwy <chenwy323@163.com>
2017-04-20 11:04:43 +0800

    v0.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
 
   resources :jobs do
     resources :resumes
